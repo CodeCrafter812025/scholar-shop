1e991ac34c30444f506f30e81260eb8f82bad59f
mohammad <ghavidelheidarei81@gmail.com>
2025-08-21 23:14:49 +0330

    توضیحی در مورد تغییرات اعمال شده

--- app/src/main/java/net/holosen/app/config/WebConfig.java
+++ app/src/main/java/net/holosen/app/config/WebConfig.java
@@ -3,6 +3,7 @@ package net.holosen.app.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -20,5 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("D:/SpringBoot-Files-10-e1a5dc4d-1958-4f78-ad1c-36fb50e7cb84/03/onlineshop/app/src/main/resources/static/images");
     }
 }
