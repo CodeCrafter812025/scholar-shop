9cd4e11435543456c4134c05a2ab25c3d4373a4e
mohammad <ghavidelheidarei81@gmail.com>
2025-09-14 00:24:57 +0330

    افزودن تصاویر محصولات و اصلاح مسیر نمایش تصاویر

--- doc/src/main/java/queries/02_file.sql
+++ doc/src/main/java/queries/02_file.sql
@@ -16,4 +16,47 @@
 --
 select * from file;
 
-UPDATE file SET content_type = 'image/webp' WHERE extension = 'webp' OR name LIKE '%.webp';
+--UPDATE file SET content_type = 'image/webp' WHERE extension = 'webp' OR name LIKE '%.webp';
+
+-- همه‌ی مسیرهایی که هنوز با 'images/' شروع نمی‌شوند، به‌روزرسانی کن
+--UPDATE file
+--SET path = CONCAT('images/', path)
+--WHERE path NOT LIKE 'images/%';
+
+-- ردیف 5: new-balance-1.webp -> jacket1.webp
+UPDATE file
+SET name = 'jacket1.webp',
+    path = 'images/jacket1.webp'
+WHERE name = 'new-balance-1.webp';
+
+-- ردیف 6: NEW-BALANCE-7.webp -> jeans1.webp
+UPDATE file
+SET name = 'jeans1.webp',
+    path = 'images/jeans1.webp'
+WHERE name = 'NEW-BALANCE-7.webp';
+
+-- ردیف 7: New-Balance-6.webp -> polo1.webp
+UPDATE file
+SET name = 'polo1.webp',
+    path = 'images/polo1.webp'
+WHERE name = 'New-Balance-6.webp';
+
+-- ردیف 8: New-Balance-4.webp -> shirt1.webp
+UPDATE file
+SET name = 'shirt1.webp',
+    path = 'images/shirt1.webp'
+WHERE name = 'New-Balance-4.webp';
+
+-- ردیف 9: new-balance-3.webp -> shorts1.webp
+UPDATE file
+SET name = 'shorts1.webp',
+    path = 'images/shorts1.webp'
+WHERE name = 'new-balance-3.webp';
+
+-- ردیف 10: white-t-shirt.jpg -> tshirt1.webp
+UPDATE file
+SET name = 'tshirt1.webp',
+    path = 'images/tshirt1.webp'
+WHERE name = 'white-t-shirt.jpg';
+
+
